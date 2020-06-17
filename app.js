378d456b7d13ea5a5ffdac35fb15f60785be6567
bensheridanedwards <bensheridanedwards@gmail.com>
2020-06-17 17:43:41 +0100

    Feat: Add bodyparsers for urlencoded and json data

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const bodyParser = require('body-parser');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
