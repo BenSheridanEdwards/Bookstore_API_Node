3a54fe562876d4e280dfd5b1248508783d254da4
bensheridanedwards <bensheridanedwards@gmail.com>
2020-06-19 16:13:20 +0100

    Feat: Able to fetch all products from database and also remove a product from the database with it's ID

--- api/routes/products.js
+++ api/routes/products.js
@@ -5,9 +5,18 @@ const mongoose = require ('mongoose')
 const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
-  res.status(200).json({
-    message: 'Handling GET requests to /products'
-  });
+  Product.find()
+    .exec()
+    .then(docs => {
+      console.log(docs);
+      res.status(200).json(docs);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
 
 router.post('/', (req, res, next) => {
@@ -22,7 +31,7 @@ router.post('/', (req, res, next) => {
       console.log(result);
       res.status(201).json({
         message: 'Handling POST requests to /products',
-        createdProduct: product,
+        createdProduct: result,
       });
     })
     .catch(err => {
@@ -39,11 +48,17 @@ router.get('/:productId', (req, res, next) => {
     .exec()
     .then(doc => {
       console.log('From database', doc);
-      res.status(200).json(doc);
+      if (doc) {
+        res.status(200).json(doc);
+      } else {
+        res.status(404).json({ message: 'No valid entry for provided ID' })
+      }
     })
     .catch(err => {
       console.log(err);
-      res.status(500).json({ error: err });
+      res.status(500).json({
+        error: err,
+      });
     });
 });
 
@@ -54,9 +69,18 @@ router.patch('/:productId', (req, res, next) => {
 });
 
 router.delete('/:productId', (req, res, next) => {
-  res.status(200).json({
-    message: 'Deleted product!',
-  });
+  const id = req.params.productId;
+  Product.remove({ _id: id })
+    .exec()
+    .then(result => {
+      res.status(200).json(result);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
 
 module.exports = router;
