2219dec8488a9bf7e2bd4fff93bd57c9378a7b46
bensheridanedwards <bensheridanedwards@gmail.com>
2020-06-25 14:34:58 +0100

    Feat: Handle DELETE requests to order route

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -88,10 +88,27 @@ router.get('/:orderId', (req, res, next) => {
 });
 
 router.delete('/:orderId', (req, res, next) => {
-  res.status(200).json({
-    message: 'Order delete',
-    orderId: req.params.orderId,
-  });
+  Order.remove({ _id: req.params.orderId })
+    .exec()
+    .then(result => {
+      res.status(200).json({
+        message: 'Order successfully deleted',
+        request: {
+          message: 'Create a new order',
+          type: 'POST',
+          url: 'http://localhost:3000/orders',
+          body: {
+            productId: 'ID',
+            quantity: 'Number',
+          },
+        },
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
 
 module.exports = router;
