595a5446610ff28f4b2efce63192789eb56ac0ae
bensheridanedwards <bensheridanedwards@gmail.com>
2020-06-30 13:26:30 +0100

    Feat: Import User model and bcrypt to user route

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,3 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const User = require('../models/user');
+
+module.export = router;
