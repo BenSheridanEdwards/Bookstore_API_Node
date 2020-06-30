6a0c11a57a6ecbba2df54b991b5ca7865ae67979
bensheridanedwards <bensheridanedwards@gmail.com>
2020-06-30 14:41:40 +0100

    Feat: Create POST request on user route that can sign up a user and find users to avoid duplicates

--- api/routes/user.js
+++ api/routes/user.js
@@ -7,12 +7,47 @@ const bcrypt = require('bcrypt');
 const User = require('../models/user');
 
 router.post('/signup', (req, res, next) => {
-  const user = new User({
-    _id: new mongoose.Types.ObjectId(),
-    email: req.body.email,
-    password: bcrypt.hash(req.body.password),
-  });
-  user.save();
+  User.find({ email: req.body.email })
+    .exec()
+    .then(user => {
+      if (user.length >= 1) {
+        return res.status(409).json({
+          message: 'Email already exists, please login',
+          url: 'http://localhost:3000/user/login',
+        });
+      }
+      bcrypt.hash(req.body.password, 10, (err, hash) => {
+        if (err) {
+          return res.status(500).json({
+            error: err,
+          });
+        }
+        const newUser = new User({
+          _id: new mongoose.Types.ObjectId(),
+          email: req.body.email,
+          password: hash,
+        });
+        newUser
+          .save()
+          .then(result => {
+            console.log(result);
+            res.status(201).json({
+              message: 'User successfully created',
+              createdUser: {
+                _id: result._id,
+                email: result.email,
+                password: 'encrypted',
+              },
+            });
+          })
+          .catch(err => {
+            console.log(err);
+            res.status(500).json({
+              error: err,
+            });
+          });
+      });
+    });
 });
 
-module.export = router;
+module.exports = router;
