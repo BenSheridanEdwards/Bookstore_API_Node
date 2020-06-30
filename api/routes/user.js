3b8614aaa27d226781b4e166e0f1dba97eb15e4a
bensheridanedwards <bensheridanedwards@gmail.com>
2020-06-30 15:53:48 +0100

    Feat: create DELETE request in user route

--- api/routes/user.js
+++ api/routes/user.js
@@ -50,4 +50,21 @@ router.post('/signup', (req, res, next) => {
     });
 });
 
+router.delete('/:userId', (req, res, next) => {
+  User.remove({ _id: req.params.id })
+    .exec()
+    .then(result => {
+      console.log(result);
+      res.status(200).json({
+        message: 'User deleted',
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+});
+
 module.exports = router;
