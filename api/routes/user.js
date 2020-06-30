8df09c1ed056d44b101dd4abd274a84386a303b1
bensheridanedwards <bensheridanedwards@gmail.com>
2020-06-30 18:15:04 +0100

    Feat: Use JWT token to authenticate login

--- api/routes/user.js
+++ api/routes/user.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -68,8 +69,19 @@ router.post('/login', (req, res, next) => {
           });
         }
         if (result) {
+          const token = jwt.sign(
+            {
+              email: user[0].email,
+              userId: user[0].userId,
+            },
+            process.env.JWT_KEY,
+            {
+              expiresIn: '1h',
+            }
+          );
           return res.status(200).json({
             message: 'Auth successful',
+            token,
           });
         }
         res.status(401).json({
