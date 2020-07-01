185c041f68401ed356c554fac647ec77b1920b87
bensheridanedwards <bensheridanedwards@gmail.com>
2020-07-01 13:39:01 +0100

    Improvement: Delete unneeded imports

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,7 +1,6 @@
 const express = require('express');
 
 const router = express.Router();
-const mongoose = require('mongoose');
 const multer = require('multer');
 const checkAuth = require('../middleware/check-auth');
 
@@ -31,8 +30,6 @@ const upload = multer({
   },
   fileFilter,
 });
-
-const Product = require('../models/product');
 
 router.get('/', ProductController.get_all);
 
