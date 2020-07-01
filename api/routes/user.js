a0779829370d50173c6771f9d2690051c4c73385
bensheridanedwards <bensheridanedwards@gmail.com>
2020-07-01 15:26:20 +0100

    Improvement: Extract User POST route, login, to controller

--- api/routes/user.js
+++ api/routes/user.js
@@ -10,51 +10,7 @@ const UserController = require('../controllers/users');
 
 router.post('/signup', UserController.signup);
 
-router.post('/login', (req, res, next) => {
-  User.find({ email: req.body.email })
-    .exec()
-    .then(user => {
-      if (user.length < 1) {
-        return res.status(401).json({
-          message: 'Auth failed',
-        });
-      }
-      bcrypt.compare(req.body.password, user[0].password, (err, result) => {
-        if (err) {
-          console.log(err);
-          return res.status(401).json({
-            message: 'Auth failed',
-            error: err,
-          });
-        }
-        if (result) {
-          const token = jwt.sign(
-            {
-              email: user[0].email,
-              userId: user[0].userId,
-            },
-            process.env.JWT_KEY,
-            {
-              expiresIn: '1h',
-            }
-          );
-          return res.status(200).json({
-            message: 'Auth successful',
-            token,
-          });
-        }
-        res.status(401).json({
-          message: 'Auth failed',
-        });
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({
-        error: err,
-      });
-    });
-});
+router.post('/login', UserController.login);
 
 router.delete('/:userId', (req, res, next) => {
   User.remove({ _id: req.params.userId })
