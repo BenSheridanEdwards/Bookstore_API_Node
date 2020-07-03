958d7d8b29623e2a16897279d98c14326cccfc92
bensheridanedwards <bensheridanedwards@gmail.com>
2020-07-03 16:12:46 +0100

    Docs: Document GET all orders route with Swagger

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -5,6 +5,23 @@ const checkAuth = require('../middleware/check-auth');
 
 const OrdersController = require('../controllers/orders');
 
+/**
+ * @swagger
+ * paths:
+ *   /orders/:
+ *     get:
+ *       tags: ['Orders']
+ *       summary: Get all orders
+ *       description: Get all orders in the database - For authorization, your need a bearer token from logging in. Insert bearer followed by your token into the authorization input box below. It should look like this "bearer yourTokenHere".
+ *       parameters:
+ *         - in: header
+ *           name: Authorization
+ *           type: string
+ *       responses:
+ *         200:
+ *           description: Receive back all orders with the productId and quantity for each order with the respective order ID.
+ */
+
 router.get('/', checkAuth, OrdersController.get_all);
 
 router.get('/:orderId', checkAuth, OrdersController.get_order);
