f3f922b982ecae473c882bd8b909dae2f1a049e7
bensheridanedwards <bensheridanedwards@gmail.com>
2020-07-03 18:21:29 +0100

    Feat: Allow to check for bearer <token> and token passed as a header on its own

--- api/middleware/check-auth.js
+++ api/middleware/check-auth.js
@@ -1,9 +1,15 @@
+/* eslint-disable prefer-destructuring */
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(' ')[1];
-    console.log(token);
+    let token;
+    const authorization = req.headers.authorization.split(' ');
+    if (authorization[1]) {
+      token = authorization[1];
+    } else {
+      token = authorization[0];
+    }
     const decoded = jwt.verify(token, process.env.JWT_KEY);
     req.userData = decoded;
     next();
