1f7182253f2153d8997f7e5a16f001e051489ec1
bensheridanedwards <bensheridanedwards@gmail.com>
2020-07-04 14:44:53 +0100

    Docs: Document order POST route, create a new order, with Swagger

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -48,6 +48,37 @@ router.get('/', checkAuth, OrdersController.get_all);
 
 router.get('/:orderId', checkAuth, OrdersController.get_order);
 
+/**
+ * @swagger
+ * paths:
+ *   /orders:
+ *     post:
+ *       tags: ['Orders']
+ *       summary: Create a new order
+ *       description: Create an order - For authorization, your need a bearer token from logging in. Enter your token into the input box below.
+ *       consumes:
+ *         - application/json
+ *       parameters:
+ *         - in: body
+ *           name: order
+ *           schema:
+ *             type: object
+ *             properties:
+ *               productId:
+ *                 type: string
+ *               quantity:
+ *                 type: integer
+ *             example:
+ *               productId: 5eecccaf6d1cec3f61544a3f
+ *               quantity: 1
+ *         - in: header
+ *           name: Authorization
+ *           type: string
+ *       responses:
+ *         201:
+ *           description: Order successfully created
+ */
+
 router.post('/', checkAuth, OrdersController.create_order);
 
 router.delete('/:orderId', checkAuth, OrdersController.delete_order);
