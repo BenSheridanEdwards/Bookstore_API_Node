495804796bed1d5a4952fb051115899be453e15c
bensheridanedwards <bensheridanedwards@gmail.com>
2020-07-07 13:03:47 +0100

    Fix: Failed app deployment

--- index.js
+++ index.js
@@ -4,44 +4,22 @@ const app = express();
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const swaggerJsDoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
 
-const http = require('http');
+const PORT = process.env.PORT || 3000;
 
-const port = 3000;
-
-const server = http.createServer(app);
-
-server.listen(port);
-
-const swaggerOptions = {
-  swaggerDefinition: {
-    info: {
-      title: 'Store RESTful API',
-      description: 'Online shop API',
-      contact: {
-        name: 'Ben Sheridan-Edwards',
-      },
-      servers: ['http://localhost:3000'],
-    },
-  },
-  apis: [
-    './api/routes/products.js',
-    './api/routes/orders.js',
-    './api/routes/user.js',
-  ],
-};
-
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 const userRoutes = require('./api/routes/user');
 
 mongoose.connect(
-  `mongodb://user:password1@ds153352.mlab.com:53352/heroku_0qlzz1r2`,
+  process.env.MONGODB_URL ||
+    `mongodb://user:password1@ds153352.mlab.com:53352/heroku_0qlzz1r2`,
   { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
 );
+
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose is connected!');
+});
 
 app.use(morgan('dev'));
 app.use('/uploads', express.static('uploads'));
@@ -61,10 +39,15 @@ app.use((req, res, next) => {
   next();
 });
 
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello World',
+  });
+});
+
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 app.use('/user', userRoutes);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use((req, res, next) => {
   const error = new Error('Not found');
@@ -80,3 +63,8 @@ app.use((error, req, res, next) => {
     },
   });
 });
+
+if (process.env.NODE_ENV === 'production') {
+}
+
+app.listen(PORT, console.log(`Server is starting at ${PORT}`));
