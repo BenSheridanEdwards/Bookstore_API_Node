106c2ec157fb09145867fb59dce9e015f66cd3a7
bensheridanedwards <bensheridanedwards@gmail.com>
2020-07-07 14:08:50 +0100

    Improve: Change user route to users routes

---
+++ api/routes/users.js
@@ -0,0 +1,126 @@
+const express = require('express');
+
+const router = express.Router();
+const checkAuth = require('../middleware/check-auth');
+
+const UserController = require('../controllers/users');
+
+/**
+ * @swagger
+ * paths:
+ *   /users/:
+ *     get:
+ *       tags: ['Users']
+ *       summary: Get all users
+ *       description: Get all users in the database
+ *       consumes:
+ *         — application/json
+ *       responses:
+ *         200:
+ *           description: Receive back all users with their email and ID.
+ */
+
+router.get('/', UserController.get_all);
+
+/**
+ * @swagger
+ * paths:
+ *   /users/signup:
+ *     post:
+ *       tags: ['Users']
+ *       summary: Create a new user
+ *       description: Create a user
+ *       consumes:
+ *         - application/json
+ *       parameters:
+ *         - in: body
+ *           name: user
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *             example:
+ *               email: test@test.com
+ *               password: password
+ *       responses:
+ *         201:
+ *           description: User successfully created
+ *         409:
+ *           description: Email already exists, please login
+ */
+
+router.post('/signup', UserController.signup);
+
+/**
+ * @swagger
+ * paths:
+ *   /users/login:
+ *     post:
+ *       tags: ['Users']
+ *       summary: Log in as an existing user
+ *       description: Login
+ *       consumes:
+ *         - application/json
+ *       parameters:
+ *         - in: body
+ *           name: user
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *             example:
+ *               email: test@test.com
+ *               password: password
+ *       responses:
+ *         200:
+ *           description: Login successful, generates bearer token.
+ *           message: Successfully logged in, please copy your bearer token below
+ *           token: bearer token
+ *         401:
+ *           description: User doesn't exist or incorrect password provided
+ *           message: Auth failed
+ */
+
+router.post('/login', UserController.login);
+
+/**
+ * @swagger
+ * paths:
+ *   /users/{userId}:
+ *     delete:
+ *       tags: ['Users']
+ *       summary: Delete a user by ID
+ *       description: Delete a user - For authorization, your need a bearer token from logging in. Enter your token into the 'Authorization' input box below.
+ *       parameters:
+ *         - in: path
+ *           name: userId
+ *           schema:
+ *             type: string
+ *           required: true
+ *         - in: header
+ *           name: Authorization
+ *           type: string
+ *       responses:
+ *         '200':
+ *           description: User successfully deleted
+ *         '404':
+ *            description: User not found.
+ *            content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  message:
+ *                    type: string
+ *                    description: No valid entry for provided ID
+ */
+
+router.delete('/:userId', checkAuth, UserController.delete_user);
+
+module.exports = router;
