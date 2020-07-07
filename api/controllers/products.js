93f26452947fd2dc19e4ab537e853f6fdfea1473
bensheridanedwards <bensheridanedwards@gmail.com>
2020-07-07 15:50:39 +0100

    Fix: Typo in url request interpolation

--- api/controllers/products.js
+++ api/controllers/products.js
@@ -17,7 +17,7 @@ exports.get_all = (req, res, next) => {
           _id: doc._id,
           request: {
             type: 'GET',
-            url: `${req.RequestUri.GetLeftPart(UriPartial.Authority)`,
+            url: `${req.RequestUri.GetLeftPart(UriPartial.Authority)}`,
           },
         })),
       });
