e9321dfbe205761ca51be0e6c21401f7aa62ac4a
bensheridanedwards <bensheridanedwards@gmail.com>
2020-07-07 15:30:11 +0100

    Improve: Add url to products in response body

--- api/controllers/products.js
+++ api/controllers/products.js
@@ -17,7 +17,7 @@ exports.get_all = (req, res, next) => {
           _id: doc._id,
           request: {
             type: 'GET',
-            url: `${process.env.PORT}`,
+            url: `${window.location.origin}/${doc._id}`,
           },
         })),
       });
@@ -48,7 +48,7 @@ exports.get_product = (req, res, next) => {
           request: {
             message: 'Get all products',
             type: 'GET',
-            url: `${process.env.PORT}`,
+            url: `${window.location.origin}/products/`,
           },
         });
       } else {
